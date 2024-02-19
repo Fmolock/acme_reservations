83e06e17bf95f3c1df806ddef6170fdf28e2c2ea
EC2 Default User <ec2-user@ip-172-31-24-210.us-east-2.compute.internal>
2024-02-19 22:49:30 +0000

    data layer is complete

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+const uuid = require('uuid');
 
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_dinner_db');
 const createTables = async ()=> {
@@ -26,7 +27,85 @@ const createTables = async ()=> {
 };
 
 
+const createCustomer = async({ name })=> {
+    const SQL = `
+        INSERT INTO customers(id, name)
+        VALUES ($1, $2)
+        RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+    return response.rows[0];
+};
+
+const createRestaurant = async({ name })=> {
+    const SQL = `
+        INSERT INTO restaurants(id, name)
+        VALUES ($1, $2)
+        RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+    return response.rows[0];
+};
+
+const fetchCustomers = async()=> {
+    const SQL = `
+        SELECT *
+        FROM customers
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+
+const fetchRestaurants = async()=> {
+    const SQL = `
+        SELECT *
+        FROM restaurants
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+
+const fetchReservations = async(customer_id)=> {
+    const SQL = `
+        SELECT *
+        FROM reservations
+        WHERE customer_id = $1
+    `;
+    const response = await client.query(SQL, [customer_id]);
+    return response.rows;
+};
+
+const destroyReservation = async(reservation) => {
+    const SQL = `
+        DELETE FROM reservations
+        WHERE id=$1 AND customer_id=$2
+    `;
+    await client.query(SQL, [reservation.id, reservation.customer_id]);
+};
+
+const createReservation = async({ reservation_date, customer_id, restaurant_id })=> {
+    const SQL = `
+        INSERT INTO reservations(id, reservation_date, customer_id, restaurant_id)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+    const response = await client.query(SQL, [
+        uuid.v4(), 
+        reservation_date,
+        customer_id,
+        restaurant_id
+    ]);
+    return response.rows[0];
+};
+
 module.exports = {
     client,
-    createTables
+    createTables,
+    createCustomer,
+    createRestaurant,
+    fetchCustomers,
+    fetchRestaurants,
+    createReservation,
+    fetchReservations,
+    destroyReservation
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const db = require('./db');
 const client = db.client;
 const createTables = db.createTables;
+const createCustomer = db.createCustomer;
+const createRestaurant = db.createRestaurant;
+const fetchCustomers = db.fetchCustomers;
+const fetchRestaurants = db.fetchRestaurants;
+const createReservation = db.createReservation;
+const fetchReservations = db.fetchReservations;
+const destroyReservation = db.destroyReservation;
 
 const init = async()=> {
     console.log('connecting to database');
@@ -8,6 +15,25 @@ const init = async()=> {
     console.log('connected to database');
     await createTables();
     console.log('tables created');
+    const [moe, lucy, ethyl, burger_king, white_castle, taco_bell, pizza_hut] = await Promise.all([
+        createCustomer({name: 'moe'}), 
+        createCustomer({name: 'lucy'}),
+        createCustomer({name: 'ethyl'}),
+        createRestaurant({name: 'burger king'}),
+        createRestaurant({name: 'white castle'}),
+        createRestaurant({name: 'taco bell'}),
+        createRestaurant({name: 'pizza hut'})
+    ]);
+    console.log(await fetchCustomers());
+    console.log(await fetchRestaurants());
+    const reservation = await createReservation({
+        reservation_date: '03/15/2024',
+        restaurant_id: white_castle.id,
+        customer_id: lucy.id
+    });
+    console.log(await fetchReservations(lucy.id));
+    await destroyReservation(reservation);
+    console.log(await fetchReservations(lucy.id));
 };
 
 init();
